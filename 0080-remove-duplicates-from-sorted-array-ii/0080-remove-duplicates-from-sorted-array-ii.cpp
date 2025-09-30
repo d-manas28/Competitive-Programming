cfaf043954ceb66364a7e689060be8520e227074
Manas Dubey <44508681+d-manas28@users.noreply.github.com>
2025-10-01 01:22:30 +0530

    Time: 0 ms (100%), Space: 19.5 MB (79.51%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int j=0;
         for(int i=0;i<nums.size();i++){
             if(i<2 || nums[i]!=nums[j-2]){
