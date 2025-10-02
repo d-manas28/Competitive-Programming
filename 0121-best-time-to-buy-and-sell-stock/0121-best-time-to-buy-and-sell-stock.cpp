d87d184bc7234e5fde35ff5de172b3f0bc0d31f6
Manas Dubey <44508681+d-manas28@users.noreply.github.com>
2025-10-02 23:08:34 +0530

    Time: 91 ms (6.45%), Space: 93.7 MB (99.99%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& nums) {
-        vector<int> suffixMax(nums.size(),-1);
-        int maxVal = INT_MIN;
-        for(int i=nums.size()-1;i>=0;i--){
-            if(nums[i]>maxVal){
-                maxVal = nums[i];
-            }
-            suffixMax[i] = maxVal;
+    int maxProfit(vector<int>& prices) {
+        int min = INT_MAX;
+        int profit = INT_MIN;
+        for(int i=0;i<prices.size();i++){
+            min = prices[i]<min?prices[i]:min;
+            profit = max(profit, prices[i]-min);
         }
-        int res = INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            res = max(res,suffixMax[i] - nums[i]);
-        }
-        return res;
+        return profit;
     }
 };
