f15a0367903c4915a8f3fedab4500e218b7b7337
Manas Dubey <44508681+d-manas28@users.noreply.github.com>
2025-10-02 23:21:00 +0530

    Time: 13 ms (6.45%), Space: 101.3 MB (5%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int maxProfit(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
         vector<int> suffixMax(nums.size(),-1);
         int maxVal = INT_MIN;
         for(int i=nums.size()-1;i>=0;i--){
