467a9af596eaad478c3bd5b70bd07972670b385d
Manas Dubey <44508681+d-manas28@users.noreply.github.com>
2025-10-06 01:07:00 +0530

    Time: 3 ms (32.15%), Space: 10.9 MB (6.16%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string convertToFormat(string s){
+        string res = "";
+        for(char i:s){
+            // if uppercase convert into lowercase
+            if(i>=65 && i<=90){
+                i = i+32;
+            }
+            // if not alphabet, ignore
+            if(isalnum(i)){
+                res += i;
+            }
+        }
+        return res;
+    }
+    bool isPalindrom(string s){
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            if(s[i]!=s[j])return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    bool isPalindrome(string s) {
+        
+        string res = convertToFormat(s);
+        cout<<res;
+        return isPalindrom(res);
+    }
+};
