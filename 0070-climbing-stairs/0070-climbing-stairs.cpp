af645998fcf5d66fa7f85e48ae2396abf0248f5e
Manas Dubey <44508681+d-manas28@users.noreply.github.com>
2025-10-07 18:47:21 +0530

    Time: 0 ms (100%), Space: 8.4 MB (51.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
-    int climbStairsHelper(int start, int end, vector<int> &dp){
-        if(start > end){
-            return 0;
-        }
-        if(start == end){
-            return 1;
-        }
-        if(dp[start] != -1){
-            return dp[start];
-        }
-        return dp[start] = climbStairsHelper(start+1,end,dp) + climbStairsHelper(start+2,end,dp);
-    }
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        return climbStairsHelper(0,n,dp);
+        vector<int> dp(n+1,-1);
+        dp[n] = 1;
+        dp[n-1] = 1;
+        for(int i=n-2;i>=0;i--){
+            dp[i] = dp[i+1] + dp[i+2];
+        }
+        return dp[0];
     }
 };
