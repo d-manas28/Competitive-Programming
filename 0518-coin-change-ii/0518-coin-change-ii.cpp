1d09f446a302e1ca10783bd9084a1c95863a8544
Manas Dubey <44508681+d-manas28@users.noreply.github.com>
2025-10-18 23:09:17 +0530

    Time: 27 ms (46.86%), Space: 36.7 MB (30.08%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int changeHelper(vector<vector<int>> &dp,int amount, vector<int> &coins, int index){
-        if(amount==0){
-            return 1;
-        }
-        
-        if(amount<0 || index>=coins.size())return 0;
-        if(dp[index][amount] != -1)return dp[index][amount];
-        dp[index][amount] = changeHelper(dp,amount-coins[index],coins,index) + changeHelper(dp,amount,coins,index+1);
-
-        return dp[index][amount];
-    } 
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size(),vector<int> (amount+1, -1));
-        int res = changeHelper(dp,amount,coins,0);
-        return res;
+        vector<vector<int>> dp(coins.size()+1,vector<int> (amount+1, 0));
+        for(int i=0;i<=coins.size();i++)
+        {
+            dp[i][0] = 1;
+        }
+        for(int i=1;i<=coins.size();i++){
+            for(int j=1;j<=amount;j++){
+                dp[i][j] = dp[i-1][j];
+                if(j-coins[i-1] >= 0){
+                    dp[i][j] = long(dp[i][j]) + long(dp[i][j-coins[i-1]]);
+                }
+            }
+        }
+        return dp[coins.size()][amount];
     }
 };
