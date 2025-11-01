18cc01c41313cc30f30d09ba83d1765be4b3e463
Manas Dubey <44508681+d-manas28@users.noreply.github.com>
2025-11-02 01:52:33 +0530

    Time: 3 ms (12.19%), Space: 13.3 MB (35.95%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,102 +1,25 @@
 class Solution {
 public:
-
-//  Recursion
-/*
-    int recursion(vector<vector<int>>& triangle, int r, int c)
-    {
-        
-        if(r == triangle.size() - 1)
-        {
-            return triangle[r][c];
-        }
-
-        
-        int diag  =  recursion(triangle, r + 1, c + 1);
-        int down    =  recursion(triangle, r + 1, c);
-
-        return triangle[r][c] + min(diag, down);
-
-    }
-*/
-
-//  Memoization
-/*
-    int memo(vector<vector<int>>& triangle, int r, int c, vector<vector<int>>& dp)
-    {
-        if(r == triangle.size() - 1)
-        {
-            return triangle[r][c];
-        }
-
-        if(dp[r][c] != -1)
-        {
-            return dp[r][c];
-        }       
-
-        int diag  =  memo(triangle, r + 1, c + 1, dp);
-        int down  =  memo(triangle, r + 1, c, dp);
-
-        dp[r][c] = triangle[r][c] + min(diag, down);
-
-        return dp[r][c];
-    }
-*/
-
-//  Tabulation
-
-    int tabu(vector<vector<int>>& triangle, int m)
-    {
-        vector<vector<int>> dp(m + 1, vector<int>(m + 1, 0));
-        
-        dp[1][1] = triangle[0][0];
-        
-        for(int i = 2; i <= m ; i++)
-        {
-            for(int j = 1; j <= i; j++)
-            {
-                if(j == 1) 
-                {
-                    dp[i][j] = triangle[i-1][j-1] + dp[i-1][j];
-                } 
-                else if(j == i) 
-                {
-                    dp[i][j] = triangle[i-1][j-1] + dp[i-1][j-1];
-                } 
-                else 
-                {
-                    dp[i][j] = triangle[i-1][j-1] + min(dp[i-1][j], dp[i-1][j-1]);
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<vector<int>> dp(triangle.size(),vector<int>(triangle.size(),0));
+        int n = triangle.size();
+        dp[0][0] = triangle[0][0];
+        int res = INT_MAX;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i+1; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + triangle[i][j];
+                } else if (i == j) {
+                    dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+                } else {
+                    dp[i][j] = min((triangle[i][j] + dp[i - 1][j - 1]),
+                                   (triangle[i][j] + dp[i - 1][j]));
+                }
+                if (i == n - 1) {
+                    res = min(res, dp[i][j]);
                 }
             }
         }
-        
-        int minPathSum = dp[m][1];
-        for (int j = 2; j <= m; ++j) 
-        {
-            minPathSum = min(minPathSum, dp[m][j]);
-        }
-        return minPathSum;    
-
-    }
-
-
-
-
-    int minimumTotal(vector<vector<int>>& triangle) {
-
-        int m = triangle.size();
-
-        // return recursion(triangle, 0, 0);
-
-        /*
-        vector<vector<int>> dp(m, vector<int>(m, -1));
-        return memo(triangle, 0, 0, dp);
-        */
-
-
-        return tabu(triangle, m);
-
-
-
+        return res==INT_MAX?dp[0][0]:res;
     }
 };
